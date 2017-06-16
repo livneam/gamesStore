b671c4f507837fdb7c3888777d3541089c8c53f6
livneam <livne.amit@gmail.com>
2017-06-16 08:55:51 +0300

    my first client files- initial commit

---
+++ app/app.js
@@ -0,0 +1,44 @@
+var myStore = angular.module("myStore",['ngRoute']);
+
+myStore.config(['$routeProvider',function($routeProvider){
+  $routeProvider
+  .when('/home',{
+    templateUrl: 'views/home.html'
+  })
+  .when('/login',{
+    templateUrl: 'views/login.html'
+  })
+  .when('/directory',{
+    templateUrl: 'views/directory.html',
+    controller: 'StoreController'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+}]);
+
+myStore.controller('StoreController',['$scope','$http',function($scope,$http){
+
+ $http.get('data/games.json').success(function(data)
+{
+  $scope.games=data;
+});
+
+ $scope.removeGame = function(game){
+    var removeGame = $scope.games.indexOf(game);
+    $scope.games.splice(removeGame,1);
+  }
+
+  $scope.addGame = function(){
+    $scope.games.push({
+      name: $scope.newgame.name,
+      belt: $scope.newgame.belt,
+      rate: parseInt($scope.newgame.rate),
+      available:true
+    });
+    $scope.newgame.name = "";
+    $scope.newgame.belt = "";
+    $scope.newgame.rate = "";
+   }
+
+}]);
